6a9f6d3bdf6601f8efa6fe32286d2d9041e22711
Di Giraldo <74432513+digiraldo@users.noreply.github.com>
2020-11-15 22:45:04 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -42,7 +42,7 @@ fi
 #Tutorial de instalación de RCone y fuse https://github.com/digiraldo/Minecraft-BE-Server/blob/main/Copias%20de%20seguridad%20en%20la%20nube.md  
 echo "Creando copia de seguridad en la nube ..."
 
-sudo rsync -avz backups/ /dirname/cloudname/
+sudo rsync -avz backups/ dirname/cloudname
 
 # Recupere la última versión del servidor dedicado Minecraft Bedrock
 echo "Buscando la última versión del servidor Minecraft Bedrock ..."
