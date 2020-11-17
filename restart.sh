b7b2004fdd9b41601ab277792ebf95a01b94490e
Di Giraldo <74432513+digiraldo@users.noreply.github.com>
2020-11-17 16:02:48 -0500

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -12,23 +12,23 @@ fi
 echo "Envío de notificaciones de reinicio al servidor..."
 
 # Iniciar aviso de cuenta atrás en el servidor
-screen -Rd servername -X stuff "say Server is restarting in 30 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 30 Segundos! $(printf '\r')"
 sleep 23s
-screen -Rd servername -X stuff "say Server is restarting in 7 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 7 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 6 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 6 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 5 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 5 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 4 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 4 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 3 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 3 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 2 seconds! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 2 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Server is restarting in 1 second! $(printf '\r')"
+screen -Rd servername -X stuff "say El Servidor se reinicia en 1 Segundos! $(printf '\r')"
 sleep 1s
-screen -Rd servername -X stuff "say Closing server...$(printf '\r')"
+screen -Rd servername -X stuff "say Cerrando servidor...$(printf '\r')"
 screen -Rd servername -X stuff "stop$(printf '\r')"
 
 echo "Cerrando servidor..."
