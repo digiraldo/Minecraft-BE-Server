f41db1341c5b7926dd4c0f5294dff65cd2a5548b
Di Giraldo <74432513+digiraldo@users.noreply.github.com>
2020-11-19 23:48:57 -0500

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -81,20 +81,6 @@ sudo sed -n "/sudo rsync -avz backups/p" start.sh
 
 sleep 5s
 
-# Montando la unidad al iniciar la maquina del servidor en crontab -e
-cd ~
-    echo -n "¿Montar la unidad $DirName/$CloudName/minecraft al iniciar la maquina? (y/n)"
-    read answer < /dev/tty
-    if [ "$answer" != "${answer#[Yy]}" ]; then
-      croncmd="$DirName/$CloudName --allow-other &"
-      # El nombre de la unidad en RClone debe ser igual al al carpeta---------------------------------------------
-      cronjob="@reboot rclone mount $CloudName: $croncmd"
-      ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
-      echo "Montaje de la Unidad programada. Para cambiar o eliminar el montaje automático, escriba crontab -e"
-    fi
-
-#@reboot rclone mount drive: /drive --allow-other &
-
 # Modificar archivo fuse.conf
 cd ~
 sudo chmod +x /etc/fuse.conf
@@ -109,17 +95,37 @@ sleep 3s
 # Iniciando Configuración Montaje de Unidad
   cd ~
   echo "Realizar el inicio de seccion de la cuenta en la nube para el Montaje del servidor $DirName/$CloudName ..."
-  
+
   sleep 4s
-  
+
   sudo rclone config
-  
+
+# Confirme el nombre de la unidad remota de rclone
+
+echo "Confirme el nombre de la unidad remota que escribio en RClone"
+
+read_with_prompt RclonName "nombre"
+
+# Montando la unidad al iniciar la maquina del servidor en crontab -e
+cd ~
+    echo -n "¿Montar la unidad $DirName/$CloudName/minecraft al iniciar la maquina? (y/n)"
+    read answer < /dev/tty
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+      croncmd="$DirName/$CloudName --allow-other &"
+      # El nombre de la unidad en RClone debe ser igual $RclonName 
+      cronjob="@reboot rclone mount $RclonName: $croncmd"
+      ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+      echo "Montaje de la Unidad programada. Para cambiar o eliminar el montaje automático, escriba crontab -e"
+    fi
+
+#@reboot rclone mount drive: /drive --allow-other &
+
 echo "Montando fuse con RClone..."
 
-echo "rclone mount $CloudName: /$DirName/$CloudName"
+echo "rclone mount $RclonName: /$DirName/$CloudName"
 
 sleep 4s
 
-sudo rclone mount drive: $croncmd
+sudo rclone mount $RclonName: $DirName/$CloudName --allow-other
 
 #sudo reboot
