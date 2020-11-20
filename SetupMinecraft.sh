2f8972a86a825d428a6ef3dea90ece9859538955
Di Giraldo <74432513+digiraldo@users.noreply.github.com>
2020-11-19 23:59:48 -0500

    Update SetupMinecraft.sh

--- SetupMinecraft.sh
+++ SetupMinecraft.sh
@@ -29,7 +29,7 @@ function read_with_prompt {
     if [[ -z ${!variable_name} ]] && [[ -n "$default" ]] ; then
       declare -g $variable_name=$default
     fi
-    echo -n "$prompt : ${!variable_name} -- accept (y/n)?"
+    echo -n "$prompt : ${!variable_name} -- aceptar? (y/n)"
     read answer < /dev/tty
     if [ "$answer" == "${answer#[Yy]}" ]; then
       unset $variable_name
