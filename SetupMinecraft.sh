91ecc6d3887bac87b09849bfa79ea9072fcb33a8
Di Giraldo <74432513+digiraldo@users.noreply.github.com>
2020-11-26 18:31:35 -0500

    Update SetupMinecraft.sh
    Muestra las configuraciones del servidor

--- SetupMinecraft.sh
+++ SetupMinecraft.sh
@@ -351,6 +351,19 @@ else
 fi
 echo "========================================================================="
 echo "========================================================================="
+echo "================CONFIGURACION PREDETERMINADA DEL SERVIDOR================"
+echo "========================================================================="
+sudo sed -n "/server-name=/p" server.properties | sed 's/server-name=/Nombre del Servidor: .... /'
+sudo sed -n "/level-name=/p" server.properties | sed 's/level-name=/Nombre del Nivel: ....... /'
+sudo sed -n "/gamemode=/p" server.properties | sed 's/gamemode=/Modo del Juego: ......... /'
+sudo sed -n "/difficulty=/p" server.properties | sed 's/difficulty=/Dificultad del Mundo: ... /'
+sudo sed -n "/allow-cheats=/p" server.properties | sed 's/allow-cheats=/Usar Trucos: ............ /'
+sudo sed -n "/max-players=/p" server.properties | sed 's/max-players=/Jugadores Máximos: ...... /'
+sudo sed -n "/white-list=/p" server.properties | sed 's/white-list=/Permiso de Jugadores: ... /'
+sudo sed -n "/level-seed=/p" server.properties | sed 's/level-seed=/Número de Semilla: ...... /'
+sudo sed -n "/server-port=/p" server.properties | sed 's/server-port=/Puerto IPV4: ............ /'
+sudo sed -n "/server-portv6=/p" server.properties | sed 's/server-portv6=/Puerto IPV6: ............ /'
+echo "========================================================================="
 sleep 3s
 
 echo "========================================================================="
